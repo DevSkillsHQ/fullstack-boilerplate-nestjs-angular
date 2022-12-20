d1ed447e693670c496d708210dcf000cdfde938c
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-12-20 12:54:41 +0100

    Creating the structure of the item component

--- app-frontend/src/app/item/item.component.ts
+++ app-frontend/src/app/item/item.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent {
-
+  @Input() transaction: any;
 }
