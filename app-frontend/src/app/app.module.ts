a527d91977aa88cb6a5e2306d0f92c19c1a5266b
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-12-20 11:18:25 +0100

    Creating main components in the project

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+import { CreateTransactionComponent } from './create-transaction/create-transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemComponent,
+    CreateTransactionComponent
   ],
   imports: [
     BrowserModule,
